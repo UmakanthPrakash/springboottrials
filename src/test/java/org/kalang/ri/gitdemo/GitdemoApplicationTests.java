8d506db85534ddaa936696543398cb23655a1538
UmakanthP <umakanth.prakash@gmail.com>
2021-06-07 12:22:25 +0400

    Spring boot first commit

---
+++ src/test/java/org/kalang/ri/gitdemo/GitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kalang.ri.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
